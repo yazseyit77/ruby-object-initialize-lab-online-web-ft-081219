5762f656ba9402b03c3bb380c4192a1cfa993231
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-21 04:32:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,11 +11,11 @@ class Dog
     @name
   end
 
-  def initialize(breed = "Mutt")
+  def initialize(breed)
     @breed = breed
   end
 
-  def breed=(breed)
+  def breed=(breed  = "Mutt")
     @breed = breed
   end
 
